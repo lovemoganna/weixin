594ba0b9963f84b693a59db2bcf5ee247cbf291e
lovemoganna <15733207536@163.com>
2018-07-01 17:14:09 +0800

    JS数据库,用来数据绑定的使用.

---
+++ data/posts-data.js
@@ -0,0 +1,103 @@
+// 可以认为它是一个本地的数据库文件
+//轮播图参数
+var local_swiper_content =
+    [
+        {swiper_image: '../../images/post/xiaolong.jpg', id: 0},
+        {swiper_image: '../../images/post/vr.png', id: 1},
+        {swiper_image: '../../images/post/cat.png', id: 3}
+
+    ]
+//新闻模块参数
+var local_post_data = [
+    {
+        avatar: '../../images/avatar/1.png',
+        date: 'Nov 28 2018',
+        title: '大闸蟹',
+        imgSrc: '../../images/post/crab.png',
+        content: '华语环境下说到宅（御宅族的宅），其实基本都是在说ACG宅，毕竟这个词发源于ACG圈子，了解过御宅族的宅的人基本上多多少少都跟ACG有所接触，以至于一般提到XX宅基本也就是XX向的ACG宅，或者是对ACG也有所爱好的XX宅。',
+        comment: '65',
+        collection: '21',
+        post_id: 0,
+        headImgSrc: '../../../images/post/crab.png',
+        detailAvatar:'../../../images/avatar/1.png',
+        author: '阿里戈多',
+        dateTime: '三天前',
+        detail: '锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.',
+        music:{
+          dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+          title: '此时此刻-许巍',
+          coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+        }
+
+        // img_condition: true
+    },
+    {
+        avatar: '../../images/avatar/1.png',
+        date: 'Nov 16 2018',
+        title: '电影时光',
+        imgSrc: '../../images/post/sls.png',
+        content: '华语环境下说到宅（御宅族的宅），其实基本都是在说ACG宅，毕竟这个词发源于ACG圈子，了解过御宅族的宅的人基本上多多少少都跟ACG有所接触，以至于一般提到XX宅基本也就是XX向的ACG宅，或者是对ACG也有所爱好的XX宅。',
+        comment: '25',
+        collection: '41',
+        post_id: 1,
+        headImgSrc: '../../../images/post/sls.png',
+        detailAvatar:'../../../images/avatar/1.png',
+        author: '阿里戈多',
+        dateTime: '三天前',
+        detail: '锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.',
+        music: {
+          dataUrl: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+          title: '此时此刻-许巍',
+          coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+        }
+        // img_condition: true
+    },
+    {
+        avatar: '../../images/avatar/1.png',
+        date: 'Nov 16 2018',
+        title: '电影时光',
+        imgSrc: '../../images/post/bl.png',
+        content: '华语环境下说到宅（御宅族的宅），其实基本都是在说ACG宅，毕竟这个词发源于ACG圈子，了解过御宅族的宅的人基本上多多少少都跟ACG有所接触，以至于一般提到XX宅基本也就是XX向的ACG宅，或者是对ACG也有所爱好的XX宅。',
+        comment: '25',
+        collection: '41',
+        post_id: 2,
+        detailAvatar:'../../../images/avatar/1.png',
+        headImgSrc: '../../../images/post/bl.png',
+        author: '胡歌',
+        dateTime: '200年前',
+        detail: '锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.',
+        music: {
+          dataUrl: 'http://m10.music.126.net/20180629164455/e1dada405730d50ee72827e839f7855c/ymusic/a3b6/0646/9cec/7cd0e353d74e56a22bc717fbc5c0d4a3.mp3',
+          title: '群星',
+          coverImgUrl: 'http://p1.music.126.net/oAIdonRUGMSewYu4lYVwHQ==/109951163240609542.jpg?param=130y130'
+        }
+        // img_condition: true
+    },
+    {
+        avatar: '../../images/avatar/1.png',
+        date: 'Nov 16 2018',
+        title: '电影时光',
+        imgSrc: '../../images/post/bl.png',
+        content: '华语环境下说到宅（御宅族的宅），其实基本都是在说ACG宅，毕竟这个词发源于ACG圈子，了解过御宅族的宅的人基本上多多少少都跟ACG有所接触，以至于一般提到XX宅基本也就是XX向的ACG宅，或者是对ACG也有所爱好的XX宅。',
+        comment: '2999',
+        collection: '41000',
+        post_id: 3,
+        detailAvatar:'../../../images/avatar/1.png',
+        headImgSrc: '../../../images/post/bl.png',
+        author: '胡默认',
+        dateTime: '200年前',
+        detail: '锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.锄禾日当午,汗滴禾下土.谁知盘中餐,粒粒皆辛苦.',
+        music: {
+            dataUrl: 'http://m10.music.126.net/20180629164455/e1dada405730d50ee72827e839f7855c/ymusic/a3b6/0646/9cec/7cd0e353d74e56a22bc717fbc5c0d4a3.mp3',
+            title: '群星',
+            coverImgUrl: 'http://p1.music.126.net/oAIdonRUGMSewYu4lYVwHQ==/109951163240609542.jpg?param=130y130'
+        }
+        // img_condition: true
+    }
+]
+
+//导出数据
+module.exports = {
+    swiper_list: local_swiper_content,
+    post_list: local_post_data
+}
